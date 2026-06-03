2a9607d708600395e0b3288ed3e5d33a0c9070c8
viswanadhmandava40-web <viswanadhmandava40@gmail.com>
2026-06-03 01:36:29 +0000

    Fixing all the issues

--- sql/00_setup_catalog_schema_volume.sql
+++ sql/00_setup_catalog_schema_volume.sql
@@ -4,6 +4,14 @@
 CREATE CATALOG IF NOT EXISTS workspace;
 CREATE SCHEMA IF NOT EXISTS workspace.bi_assignment;
 
+-- DROP TABLE workspace.bi_assignment.bronze_customer;
+-- DROP TABLE workspace.bi_assignment.bronze_orders;
+-- DROP TABLE workspace.bi_assignment.dim_customer;
+-- DROP TABLE workspace.bi_assignment.bronze_category;
+-- DROP TABLE workspace.bi_assignment.bronze_product;
+-- DROP TABLE workspace.bi_assignment.dim_product;
+-- DROP TABLE workspace.bi_assignment.fact_order_line;
+
 -- Create the assignment volume if you have Unity Catalog volume permissions.
 CREATE VOLUME IF NOT EXISTS workspace.bi_assignment.etl_assignment;
 
